2e301de71063c37cbefe2914dce3e7d8e5a041ee
Jordan Yu <jordanyu44@gmail.com>
2018-08-25 16:05:47 -0700

    Set up user and events test routes

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const users = require('./routes/api/users');
+const events = require('./routes/api/events');
 const db = require('./config/keys').mongoURI;
+const app = express();
+const bodyParser = require('body-parser');
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err))
+  .connect(db,{ useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Sup, foo"));
+app.use("/api/users", users);
+app.use("/api/events", events); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
