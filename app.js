b6c8ae561b413dc29ad6a549f28494ca5b29de1e
Jordan Yu <jordanyu44@gmail.com>
2018-08-25 16:34:02 -0700

    Build user registration

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("Sup, foo"));
 app.use("/api/users", users);
 app.use("/api/events", events); 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
